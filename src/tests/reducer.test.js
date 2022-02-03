b1a75e412bb6359143c35967e96171ab2d4de46f
emmanuel <gbengacode>
2022-02-03 13:34:19 +0100

    Add test cases

---
+++ src/tests/reducer.test.js
@@ -0,0 +1,12 @@
+import covidReducer  from '../redux/covid/covid'
+import data from './__mock__/mock'
+test('should return the initial state', () => {
+  
+    expect(covidReducer(undefined, {})).toEqual([])
+})
+
+
+test('should return result from api call', () => {
+    
+    expect(covidReducer(data, {})).toHaveLength(1)
+})
