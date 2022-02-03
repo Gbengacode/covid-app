4f37bda4930da4e3e9eaec435f62949a142fc24d
emmanuel <gbengacode>
2022-02-03 13:49:22 +0100

    Fix linter errors

--- src/tests/card.test.js
+++ src/tests/card.test.js
@@ -1,45 +1,48 @@
 import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
-import Card from '../pages/Card'
+import Card from '../pages/Card';
 import '@testing-library/jest-dom';
 
-   it('Should be rendering text', () => {
-     const Data = [
-       {
-         id: 1,
-         name: "Missouri",
-         figure: 2345
-       },
-       {
-        id: 2,
-        name: "Maryland",
-        figure: 2345
-       }
-     ]
-    render(<BrowserRouter><Card id={Data[0].id} name={Data[0].name} figure={Data[0].figure} /></BrowserRouter>)
-    expect(screen.getByText(/Missouri/i)).toBeInTheDocument();
-  });
+it('Should be rendering text', () => {
+  const Data = [
+    {
+      id: 1,
+      name: 'Missouri',
+      figure: 2345,
+    },
+    {
+      id: 2,
+      name: 'Maryland',
+      figure: 2345,
+    },
+  ];
+  render(
+    <BrowserRouter>
+      <Card
+        id={Data[0].id}
+        name={Data[0].name}
+        figure={Data[0].figure}
+      />
+    </BrowserRouter>,
+  );
+  expect(screen.getByText(/Missouri/i)).toBeInTheDocument();
+});
 
-
-    
-  it('should display filter data', () => {
-    const Data = [
-      {
-        id: 1,
-        name: "Missouri",
-        figure: 2345
-      },
-      {
-       id: 2,
-       name: "New york",
-       figure: 2345
-      }
-    ]
-    const hl = Data.filter(data => data.name.includes('Missouri')).map(infor => infor.name)
-    const number = 2345;
-    render(<BrowserRouter><Card id="1" name={hl[0]} figure={number}/></BrowserRouter>)
-    expect(screen.getByText(/Missouri/i)).toBeInTheDocument();
-  })
-
-
-  
+it('should display filter data', () => {
+  const Data = [
+    {
+      id: 1,
+      name: 'Missouri',
+      figure: 2345,
+    },
+    {
+      id: 2,
+      name: 'New york',
+      figure: 2345,
+    },
+  ];
+  const hl = Data.filter((data) => data.name.includes('Missouri')).map((infor) => infor.name);
+  const number = 2345;
+  render(<BrowserRouter><Card id="1" name={hl[0]} figure={number} /></BrowserRouter>);
+  expect(screen.getByText(/Missouri/i)).toBeInTheDocument();
+});
--- src/tests/reducer.test.js
+++ src/tests/reducer.test.js
@@ -1,12 +1,10 @@
-import covidReducer  from '../redux/covid/covid'
-import data from './__mock__/mock'
-test('should return the initial state', () => {
-  
-    expect(covidReducer(undefined, {})).toEqual([])
-})
+import covidReducer from '../redux/covid/covid';
+import data from './__mock__/mock';
 
+test('should return the initial state', () => {
+  expect(covidReducer(undefined, {})).toEqual([]);
+});
 
 test('should return result from api call', () => {
-    
-    expect(covidReducer(data, {})).toHaveLength(1)
-})
+  expect(covidReducer(data, {})).toHaveLength(1);
+});
